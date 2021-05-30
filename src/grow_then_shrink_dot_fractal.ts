741bf091289549f4e3c0806bc90ffb39b1dde07f
Matt Dailey <mjd@figma.com>
2021-05-30 12:18:56 -0700

    change color when note hits

--- src/grow_then_shrink_dot_fractal.ts
+++ src/grow_then_shrink_dot_fractal.ts
@@ -3,8 +3,9 @@ import { GrowingDotRenderer } from "./growing_dot_fractal"
 import { ShrinkingDotRenderer } from "./shrinking_dot_fractal"
 import { Engine } from "./engine"
 import { RadialCollisionDetector, RadialRandomWalk } from "./new_point_algos/random_walk"
-import { Renderer } from "./models"
+import { FillProvider, Renderer } from "./models"
 import { WhiteFillProvider } from "./white_fill_provider"
+import { MidiFillProvider } from "./midi_fill_provider"
 
 
 export function growThenShrinkEngine(canvas: HTMLCanvasElement) {
@@ -12,13 +13,16 @@ export function growThenShrinkEngine(canvas: HTMLCanvasElement) {
   const addTimeMs = 1000
   const growTimeMs = 1000
   const shrinkTimeMs = 1000
+  const colorFadeTime = 500
 
   const providers: Array<Renderer> = []
 
+  const fillProvider = new MidiFillProvider(colorFadeTime)
+
   for (let i = 0; i < 10; i++) {
     providers.push(
-      new GrowingDotRenderer(canvas, radius, new WhiteFillProvider(), growTimeMs, addTimeMs),
-      new ShrinkingDotRenderer(canvas, radius, new WhiteFillProvider(), shrinkTimeMs)
+      new GrowingDotRenderer(canvas, radius, fillProvider, growTimeMs, addTimeMs),
+      new ShrinkingDotRenderer(canvas, radius, fillProvider, shrinkTimeMs)
     )
   }
 
@@ -29,6 +33,10 @@ export function growThenShrinkEngine(canvas: HTMLCanvasElement) {
     renderProvider,
     canvas
   )
+  fillProvider.startListeningToMidiBus()
+
+  engine.addPoint({ x: canvas.width / 2, y: canvas.height / 2 })
+  engine.run(9)
 
   return engine
 }
---
+++ src/midi_fill_provider.ts
@@ -0,0 +1,53 @@
+import { FillProvider, Point } from "./models"
+import WebMidi from 'webmidi'
+
+export class MidiFillProvider implements FillProvider {
+
+  private static colors = [
+    '#CC99C9',
+    '#9EC1CF',
+    '#9EE09E',
+    '#FDF097',
+    '#FEB144',
+    '#FF6663',
+  ]
+
+  // initialize -1 so first note goes to first color in list
+  private colorIndex: number = -1
+  private mostRecentNoteMs: number|null = null
+
+  constructor(private decayTimeMs: number) {}
+
+  public startListeningToMidiBus() {
+    const parentProvider = this
+    WebMidi.enable(function (err) {
+      if (err) {
+        console.log("WebMidi could not be enabled.", err);
+      } else {
+        console.log("WebMidi enabled!");
+        WebMidi.inputs[1].addListener('noteon', 'all', (e) => {
+          parentProvider.colorIndex = (parentProvider.colorIndex+1) % MidiFillProvider.colors.length
+          parentProvider.mostRecentNoteMs = Date.now()
+          console.log(e)
+        })
+      }
+    });
+  }
+
+  getFill(point: Point, index: number, length: number): string {
+    if (!this.mostRecentNoteMs) {
+      console.log("no fill")
+      return '#ffffff00'
+    }
+
+    const timeSinceNoteMs = Date.now() - this.mostRecentNoteMs
+    if (timeSinceNoteMs < this.decayTimeMs) {
+      const alpha = Math.round(100 * ((this.decayTimeMs - timeSinceNoteMs) / this.decayTimeMs))
+      const color = MidiFillProvider.colors[this.colorIndex]
+      return `${color}${alpha.toString(16)}`
+    }
+
+    return '#ffffff00'
+  }
+
+}
